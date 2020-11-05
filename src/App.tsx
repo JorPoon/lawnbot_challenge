7372fa962a9cb0fcd663f1785ba7eba7a344102d
JorPoon <jormpoon@gmail.com>
2020-11-04 21:35:56 -0500

    added themeprovider and css reset onto root file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Box } from "@chakra-ui/core";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Box bg="tomato" w="100%" p={4} color="white">
+        This is the Box
+      </Box>
     </div>
   );
 }
